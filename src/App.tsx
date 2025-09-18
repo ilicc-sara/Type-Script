d4861da1c7009f672d4ad995f77ad14c425c0c84
Sara <sarailic431@gmail.com>
2025-09-18 18:48:02 +0200

    . . . ..  . . ......

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,18 @@
+import { useReducer, useState } from "react";
 import "./App.css";
 
 function App() {
-  return <></>;
+  const reducer = (count: number, newValue) => {
+    return newValue;
+  };
+
+  const [count, setCount] = useReducer(reducer, 0);
+  const [draftCount, setDraftCount] = useState(count);
+  return (
+    <>
+      <h1 className="text-red-500">Type Script Course</h1>
+    </>
+  );
 }
 
 export default App;
