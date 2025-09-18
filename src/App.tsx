e1dcd62fa2747cc919ce2887151424a0f7a2b1c1
Sara <sarailic431@gmail.com>
2025-09-18 21:41:55 +0200

    . . . . . ......

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,10 @@
-import { useReducer, useState, ActionDispatch, Dispatch } from "react";
+import { useReducer } from "react";
 import "./App.css";
 
 function App() {
   type InitialState = {
     count: number;
-    draftCount: string | number;
+    draftCount: number;
   };
 
   const initialState: InitialState = {
@@ -12,80 +12,66 @@ function App() {
     draftCount: 0,
   };
 
-  type Action = {
-    type: "increment" | "decrement" | "reset" | "updateCountFromDraft";
-  };
+  type Action =
+    | { type: "increment" }
+    | { type: "decrement" }
+    | { type: "reset" }
+    | { type: "updateCountFromDraft" }
+    | { type: "updateDraftCount"; payload: number };
 
-  type ActionWithPayload {
-    type: "updateDraftCount",
-    payload: number
-  }
-
-  const reducer = (state = initialState, action: Action | ActionWithPayload) => {
+  const reducer = (state: InitialState, action: Action): InitialState => {
     const { count, draftCount } = state;
 
-
-
-    if (action.type === "increment") {
-      const newCount = count + 1;
-      return { count: newCount, draftCount: newCount };
+    switch (action.type) {
+      case "increment":
+        const inc = count + 1;
+        return { count: inc, draftCount: inc };
+      case "decrement":
+        const dec = count - 1;
+        return { count: dec, draftCount: dec };
+      case "reset":
+        return { count: 0, draftCount: 0 };
+      case "updateDraftCount":
+        return { count, draftCount: action.payload };
+      case "updateCountFromDraft":
+        return { count: Number(draftCount), draftCount };
+      default:
+        return state;
     }
-
-     
-
-    if (action.type === "decrement") {
-      const newCount = count - 1;
-      return { count: newCount, draftCount: newCount };
-    }
-
-    if (action.type === "reset") {
-      return { count: 0, draftCount: 0 };
-    }
-
-    if (action.type === "updateDraftCount") {
-      console.log("updateDraftCount");
-
-      return { count, draftCount: action.payload };
-    }
-
-    if (action.type === "updateCountFromDraft") {
-      return { count: Number(draftCount), draftCount };
-    }
-
-    return state;
   };
 
   const [state, dispatch] = useReducer(reducer, initialState);
 
-  // setCount(4);
   return (
     <section className="flex flex-col items-center w-2/3 gap-8 p-8 bg-white border-4">
-      <h1>Days Since the Last Accindent</h1>
-      <p className="text-6xl"> {state.count} </p>
+      <h1>Days Since the Last Accident</h1>
+      <p className="text-6xl">{state.count}</p>
       <div className="flex gap-2">
         <button onClick={() => dispatch({ type: "decrement" })}>
-          {" "}
           ➖ Decrement
         </button>
-        <button onClick={() => dispatch({ type: "reset" })}> 🔁 Reset </button>
+        <button onClick={() => dispatch({ type: "reset" })}>🔁 Reset</button>
         <button onClick={() => dispatch({ type: "increment" })}>
-          {" "}
-          ➕ Increment{" "}
+          ➕ Increment
         </button>
       </div>
       <div>
         <form
           onSubmit={(e) => {
             e.preventDefault();
-            dispatch({ action: "updateDraftCount" });
+            dispatch({ type: "updateCountFromDraft" });
           }}
         >
           <input
             type="number"
             value={state.draftCount}
-            onChange={}
+            onChange={(e) =>
+              dispatch({
+                type: "updateDraftCount",
+                payload: Number(e.target.value),
+              })
+            }
           />
-
           <button type="submit">Update Counter</button>
         </form>
       </div>
