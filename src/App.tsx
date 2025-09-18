ac74462d273dc403748944ac5fbcc03b37b8dc89
Sara <sarailic431@gmail.com>
2025-09-18 20:27:34 +0200

    . . . . . . . ........

--- src/App.tsx
+++ src/App.tsx
@@ -42,16 +42,39 @@ function App() {
     return state;
   };
 
-  const [count, setCount] = useReducer(reducer, 0);
-  const [draftCount, setDraftCount] = useState(count);
+  const [state, dispatch] = useReducer(reducer, initialState);
 
   // setCount(4);
   return (
     <section className="flex flex-col items-center w-2/3 gap-8 p-8 bg-white border-4">
       <h1>Days Since the Last Accindent</h1>
-      <p className="text-6xl"> {count} </p>
+      <p className="text-6xl"> {state.count} </p>
       <div className="flex gap-2">
-        <button onClick={() => setCount((count) => count - 1)}>button</button>
+        <button onClick={() => dispatch({ type: "decrement" })}>
+          {" "}
+          ➖ Decrement
+        </button>
+        <button onClick={() => dispatch({ type: "reset" })}> 🔁 Reset </button>
+        <button onClick={() => dispatch({ type: "increment" })}>
+          {" "}
+          ➕ Increment{" "}
+        </button>
+      </div>
+      <div>
+        <form
+          onSubmit={(e) => {
+            e.preventDefault();
+            dispatch({ action: "updateDraftCount" });
+          }}
+        >
+          <input
+            type="number"
+            value={draftCount}
+            onChange={(e) => setDraftCount(e.target.valueAsNumber)}
+          />
+
+          <button type="submit">Update Counter</button>
+        </form>
       </div>
     </section>
   );
