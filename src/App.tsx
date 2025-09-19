42fb254ee5f13c81f9f2c204b5eb38e7c331ab4c
Sara <sarailic431@gmail.com>
2025-09-19 19:21:57 +0200

    . . . . . . . . . . . . . ......

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,24 @@
 import "./App.css";
+import { useState } from "react";
+import SavedColors from "./saved-colors";
+import RelatedColors from "./related-colors";
+import AdjustColors from "./adjust-colors";
+import ColorPicker from "./color-picker";
 
-function App() {
+const App = () => {
+  const [hexColor, setHexColor] = useState("#e56e24");
+
   return (
-    // <section className="flex flex-col items-center w-2/3 gap-8 p-8 bg-white border-4"></section>
-    <section></section>
+    <div className="grid max-w-3xl grid-cols-1 gap-8 p-8 pb-40 mx-auto dark:bg-slate-900 dark:text-white sm:grid-cols-2">
+      <ColorPicker
+        hexColor={hexColor}
+        onChange={(e) => setHexColor(e.target.value)}
+      />
+      <AdjustColors hexColor={hexColor} />
+      <RelatedColors hexColor={hexColor} />
+      <SavedColors hexColor={hexColor} />
+    </div>
   );
-}
+};
 
 export default App;
