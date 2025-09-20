6db58f8680d42aa97d5ddb5c653637da6b537a09
Sara <sarailic431@gmail.com>
2025-09-20 21:23:07 +0200

    adding types to prop

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,13 @@
 import { useState } from "react";
 import "./App.css";
-
-type User = {
-  name: string;
-  age: number | string | boolean;
-  isAdmin?: boolean;
-};
-
-type UserAdmin = User & {
-  adminId: string;
-};
+import Card from "./Card";
 
 const App = () => {
-  function doSomething(x: (string | number)[]) {
-    x[1] = true;
-  }
-
-  return <></>;
+  return (
+    <>
+      <Card text={"Hello"} count={0} />
+    </>
+  );
 };
 
 export default App;
