53495cbf8ef6911080dcdc26c757cb6a7360233f
Sara <sarailic431@gmail.com>
2025-09-20 22:11:56 +0200

    passing functions as type props

--- src/App.tsx
+++ src/App.tsx
@@ -3,10 +3,14 @@ import "./App.css";
 import Card from "./Card";
 
 const App = () => {
-  const [count, setCount] = useState(0);
+  // const [count, setCount] = useState(0);
+
+  function alertMessage(message: string) {
+    alert(message);
+  }
   return (
     <>
-      <Card setCount={setCount} />
+      <Card alertMessage={alertMessage} />
     </>
   );
 };
--- src/Card.tsx
+++ src/Card.tsx
@@ -1,20 +1,13 @@
 import React from "react";
 
 type CardProps = {
-  // text: string;
-  // count?: number;
-  // children?: React.ReactNode;
-  setCount: React.Dispatch<React.SetStateAction<number>>;
+  alertMessage: (message: string) => void;
 };
 
-// function Card({ text, count }: CardProps) {
-function Card({ setCount }: CardProps) {
+function Card({ alertMessage }: CardProps) {
   return (
     <div>
-      <button onClick={() => setCount((count: number) => count + 1)}>
-        {" "}
-        Increase Count
-      </button>
+      <button onClick={() => alertMessage("Alert Message")}> Alert</button>
     </div>
   );
 }
